665769ebd2f7a5bedbcf140941dcdc0588b3d4c1
Tanushreehurali <tanuhurali@gmail.com>
2025-10-23 14:35:13 +0530

    Update simple_int_1.py

--- simple_int_1.py
+++ simple_int_1.py
@@ -1,8 +1,11 @@
-
 P = 10000  
 R = 5      
-T = 3      
+T = 3     
 
 SI = (P * R * T) / 100
 
+
+CI = P * ((1 + R / 100) ** T) - P
+
 print(f"Simple Interest: ₹{SI}")
+print(f"Compound Interest: ₹{round(CI, 2)}")
